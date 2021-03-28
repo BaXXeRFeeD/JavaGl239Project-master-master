68eb627e0897de85c24070c23f3bd78f938d91ea
BaXXeRFeeD <43886486+BaXXeRFeeD@users.noreply.github.com>
2021-03-28 21:54:09 +0300

    Update Rectangle.java

--- src/main/java/problem/Rectangle.java
+++ src/main/java/problem/Rectangle.java
@@ -20,7 +20,7 @@ public class Rectangle {
     void render(GL2 gl){
         gl.glPointSize(3);
         gl.glColor3d(1,0.5,0);
-        gl.glLineWidth(3);
+        gl.glLineWidth(2);
         gl.glBegin(GL.GL_LINE_LOOP);
         gl.glVertex2d(a.x,a.y);
         gl.glVertex2d(b.x,b.y);
